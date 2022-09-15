02aa9bd6c47c9c9cc719b22af12b22d43964f798
shalini kumari <shalini05051998@gmail.com>
2022-09-15 22:14:40 +0530

    done

--- block-BJaaco/code/index.js
+++ block-BJaaco/code/index.js
@@ -3,11 +3,42 @@ let numbers = [6, 8, 10, 12, 43, 56, 98];
 let userIds = [1230, 234, 1278, 984, 763, 900];
 
 // 1. Add all the values of numbers and userIds array into the new newly created array named `collection`
+let sumOfNumber = 0;
+for (let number of numbers) {
+  sumOfNumber += number;
+}
+let sumOfUserIds = 0;
+for (let Id of userIds) {
+  sumOfUserIds += Id;
+}
+
+let  collection = [sumOfNumbers, sumOfUserIds];
+console.log(collection);
 
 // 2. Add all the even numbers from both arrays numbers and userIds into a newly created array named `evenCollection`
-
+let evenCollection = []
+for (let number of numbers) {
+  if (number % 2 == 0) {
+    evenCollection.push(number)
+  }
+}
+for(let number of userIds){
+if(number%2 ==0){
+  evenCollection.push(number)
+}
+}
 // 3. Add all the odd numbers from both arrays numbers and userIds into a newly created array named `oddCollection`
-
+let oddCollection = []
+for (let number of numbers) {
+  if (number % 2 !== 0) {
+    oddCollection.push(number)
+  }
+}
+for(let number of userIds){
+if(number%2 !==0){
+  oddCollection.push(number)
+}
+}
 /*
   @param means parameter
 
